50c674840589cc769b03a5d8c316e0af50361771
qinyueshang <yue_hong_y@163.com>
2017-11-11 21:05:50 +0800

    modified

--- webpack.config.js
+++ webpack.config.js
@@ -2,35 +2,42 @@ var path = require('path');
 var webpack = require('webpack');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var CleanWebpackPlugin = require('clean-webpack-plugin');
+
 var production = process.env.NODE_ENV === 'production'
-var plugins = [];
-var loader='style!css!sass';
-if(production) {
-plugins = [
-		new webpack.optimize.UglifyJsPlugin(),
-	  new ExtractTextPlugin('style.css'),
-	  new HtmlWebpackPlugin({ //根据模板插入css/js等生成最终HTML
-	            filename: './index.html', //生成的html存放路径，相对于path
-	            template: './index.html', //html模板路径
-	            inject: 'body', //js插入的位置，true/'head'/'body'/false
-	            hash: true, //为静态资源生成hash值
-	            //chunks: ['bundle'],//需要引入的chunk，不配置就会引入所有页面的资源
-	            minify: { //压缩HTML文件
-	                removeComments: true, //移除HTML中的注释
-	                collapseWhitespace: false //删除空白符与换行符
-	            }
-	})
+var loader='style-loader!css-loader!sass-loader';
+var filename='[name].js';
+var publicPath='/'
+var plugins = [
+	new HtmlWebpackPlugin({ //根据模板插入css/js等生成最终HTML
+			 filename: './index.html', //生成的html存放路径，相对于path
+			 template: './index.html', //html模板路径
+			 inject: 'body', //js插入的位置，true/'head'/'body'/false
+			 chunks: ['index'],//需要引入的chunk，不配置就会引入所有页面的资源
+			 minify: { //压缩HTML文件
+					 removeComments: true, //移除HTML中的注释
+					 collapseWhitespace: false //删除空白符与换行符
+			 }
+})
 ];
-loader=ExtractTextPlugin.extract('css!sass');
+if(production) {
+plugins =plugins.concat([
+	  new CleanWebpackPlugin(['public']),
+		new webpack.optimize.UglifyJsPlugin(),
+	  new ExtractTextPlugin({ filename: 'css/[name].[contenthash].css', disable: false, allChunks: true }),
+
+]);
+loader=ExtractTextPlugin.extract('css-loader!sass-loader');
+filename='js/[name].[chunkhash].js';
+publicPath=''
 }
 
-
 module.exports = {
-	entry: './app/index.js',
+	entry:{index: './app/index.js'},
 	output: {
-		path: path.join(__dirname, 'public/'),
-		filename: 'bundle.js',
-		publicPath: '/',
+		path: path.join(__dirname, 'public'),
+		filename: filename,
+		publicPath:publicPath,
 	},
 	module: {
 		loaders: [
@@ -38,13 +45,14 @@ module.exports = {
 				test: /\.js$/,
 				exclude: /node_modules/,
 				loader: 'babel-loader',
-				query: {presets: ['es2015', 'react']}
+				query:{presets: ['es2015', 'react']}
 			},
 			{
 				test: /.scss$/,
 				exclude: /node_modules/,
 				loader: loader
-			}
+			},
+
 		]
 	},
   plugins: plugins,
@@ -59,7 +67,7 @@ module.exports = {
         }
       }
     }*/
-		contentBase: './',
+		contentBase: '/',
 		host: 'localhost',
 		port: 9090, //默认8080
 		inline: true, //可以监控js变化
